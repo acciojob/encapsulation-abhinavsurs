6b63fcc5c05bb0bb6e2e97cfc1b0e37d27e98a78
abhinavsurs <abhinavsuresh.11@gmail.com>
2022-10-06 20:46:07 +0530

    commit for encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj1=new RWOnly();
+       // java: name has private access in com.driver.RWOnly
+        obj1.setter("abhinav");
+        obj1.getter();
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class RWOnly {
+    private String name;
+
+    public void setter(String str){
+        this.name =str;
+    }
+    public String getter(){
+        return name;
+    }
     
 }
